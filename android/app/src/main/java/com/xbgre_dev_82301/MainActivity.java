bd887c7cd32b16bbe7d8893ada975f70c3029322
Crowdbotics <engineering@crowdbotics.com>
2023-05-11 05:59:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/xbgre_dev_82301/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xbgre_dev_82301;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "xbgre_dev_82301";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "xbgre-dev-82301",
+  url: "https://xbgre-dev-82301.botics.co",
+  api: "https://xbgre-dev-82301.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
